1c506d03c3c160a0cd1b311e51afa6c1fdee3412
Gaurav Saxena <sgaurav8819@gmail.com>
2022-04-14 09:43:50 +0530

    funtion practice session

---
+++ src/function_prac.py
@@ -0,0 +1,10 @@
+# to define a function indenation must be taken care of.
+# working with parameter
+def say_hi(name, age):
+  print("Hello "+ name + " you are "+age)
+
+say_hi("Mike", "25")
+say_hi("Steve", "50")
+
+
+
