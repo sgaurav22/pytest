35aa3e892214719e981351d85b0953c462e9deb2
Gaurav Saxena <sgaurav8819@gmail.com>
2022-04-14 09:28:36 +0530

    tuples practice session

---
+++ src/tuples.py
@@ -0,0 +1,4 @@
+# tuples can be created using round brackets and their values can't be modified once created
+coordinate = (4, 5)
+print(coordinate[0])
+coordinate[0] = 20  # this line should give error as we trying to update its value
