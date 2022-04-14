7ab5aa0885bf875e4a61500ebc92604dfc95e7c4
Gaurav Saxena <sgaurav8819@gmail.com>
2022-04-14 11:05:15 +0530

    dictionary practice session

---
+++ src/dict.py
@@ -0,0 +1,11 @@
+# dictionaries are same as map. which stores key value pair
+
+monthConversion = {
+  "Jan" : "January",
+  "Feb" : "Febuary",
+  "Mar" : "March"
+}
+
+print(monthConversion["Jan"])
+
+print(monthConversion.get("Feb"))
