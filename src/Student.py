9f5042412529355f49cc304fae60f067fb03a18f
Gaurav Saxena <sgaurav8819@gmail.com>
2022-04-15 22:14:14 +0530

    classes n objects to be done.

---
+++ src/Student.py
@@ -0,0 +1,8 @@
+class Student:
+
+  def __init__(self, id, name, major, gpa, is_on_probabtion):
+    self.id = id
+    self.name = name
+    self.major = major
+    self.gpa = gpa
+    self.is_on_probabtion = is_on_probabtion
---
+++ src/app.py
@@ -0,0 +1,4 @@
+from Student import Student
+
+stud1 = Student(1, "Gaurav", "Physics")
+print(stud1.id)
