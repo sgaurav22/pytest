988dadfc6701f4bb3d553fd98f69a6cd5422d9a5
Gaurav Saxena <sgaurav8819@gmail.com>
2022-04-15 21:05:23 +0530

    read file practice_1

--- src/read_file.py
+++ src/read_file.py
@@ -1,14 +1,19 @@
-employee_file = open("employee", "r")
-#print(employee_file.readable())
+try:
+  employee_file = open("employee", "r")
+  #print(employee_file.readable())
 
-#for employee in employee_file.readlines():
-  #print(employee)
-emp_name = []
-emp_des = []
-for employee in employee_file.readlines():
-  emp = employee.split(",")
-  emp_name.append(emp[0])
-  emp_des.append(emp[1].strip())
+  #for employee in employee_file.readlines():
+    #print(employee)
+  emp_name = []
+  emp_des = []
+  for employee in employee_file.readlines():
+    emp = employee.split(",")
+    emp_name.append(emp[0].strip())
+    emp_des.append(emp[1].strip())
 
-print(emp_name)
-print(emp_des)
+  print(emp_name)
+  print(emp_des)
+except FileNotFoundError as err:
+  print(err)
+finally:
+  employee_file.close()
