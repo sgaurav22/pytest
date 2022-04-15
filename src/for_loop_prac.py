e407602993b35ea4a476d413816e5d2b7b75deb7
Gaurav Saxena <sgaurav8819@gmail.com>
2022-04-15 17:54:31 +0530

    for loop practice

---
+++ src/for_loop_prac.py
@@ -0,0 +1,16 @@
+# for each loop example
+for letter in "Gaurav Saxena":
+  print(letter.upper())
+
+
+#num_list = [1, 2, 3, 4, 5]
+#len = len(num_list)
+#for num in num_list:
+#  print(num * len)
+
+num_lists = [1, 2, 3, 4, 5]
+#nums = len(num_list)
+
+for index in range(len(num_lists)):
+  print(num_lists[index])
+
