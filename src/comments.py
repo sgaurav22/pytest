558d4667ef381f1ff449e06a19eddf1900a5233c
Gaurav Saxena <sgaurav8819@gmail.com>
2022-04-15 18:38:52 +0530

    comments practice

---
+++ src/comments.py
@@ -0,0 +1,10 @@
+
+# Single line comments is used for notes or giving info about the block of code
+print("Single line comments.")
+
+'''
+This is used for multi line comments
+but the single line comment is the preffred way of
+doing comments.
+'''
+print("Multiline comments.")
