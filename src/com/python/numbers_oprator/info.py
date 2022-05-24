41faa9e44f2b2775a8d795837267e526c3e337b8
gaurav <saxenagau@gmail.com>
2022-05-24 16:25:14 +0530

    Updated the basic info.

---
+++ src/com/python/numbers_oprator/info.py
@@ -0,0 +1,22 @@
+#
+# => Type of Datatype <= #
+# Number => 1,2,3,4,5
+# Floating Point => 1.0,2.0,3.0,4.5,5.6
+# Boolean => True and False
+# Comments => # is used to comment the code
+# Operator => +, -, /,*, **, ==, >
+# Exception => SyntaxError
+#
+# => Type of Operator <= #
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
